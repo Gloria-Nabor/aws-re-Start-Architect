9217ad7599c684df5b6be5952ba251e667dd2a06
Gloria Nabor <114413852+Gloria-Nabor@users.noreply.github.com>
2023-10-30 21:29:56 -0600

    Add files via upload
    Trabajando con bucles
    
    Un bucle es un segmento de código que se repite. 
    
    En este laboratorio, aprenderá a utilizar un bucle while.

---
+++ program-3.py
@@ -0,0 +1,22 @@
+# Trabajo con un bucle while
+print("Welcome to Guess the Number!")
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+
+import random
+
+number = random.randint(1,10)
+
+isGuessRight = False
+
+# El bucle while repetirá el código hasta que se adivine el número correcto
+# Python utiliza la sangría con espacios para determinar los bloques lógicos, es decir, qué instrucciones se consideran parte del bucle
+
+while isGuessRight != True:
+    guess = input("Guess a number between 1 and 10: ")
+    if int(guess) == number:
+        print("You guessed {}. That is correct! You win!".format(guess))
+        isGuessRight = True
+    else:
+        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
+        
+        
